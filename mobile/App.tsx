0a744d7351951dfff04adaa68f87baae68c74f0e
Jason van den Berg <jayvdb1@gmail.com>
2022-11-15 22:14:01 +0200

    mobile first add

---
+++ mobile/App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar
+        barStyle={'light-content'}
+      />
+      <Text>LnPush</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    color: 'red'
+  }
+});
+
+export default App;
