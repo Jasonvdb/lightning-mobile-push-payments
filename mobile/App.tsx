3bd1051a4637ece8ba78ec92d977804814623772
Jason van den Berg <jayvdb1@gmail.com>
2022-11-16 22:55:55 +0200

    Basic lightning wallet setup

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,56 +1,363 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
-import React, {useEffect, type PropsWithChildren} from 'react';
+import './shim';
+import React, { ReactElement, useEffect, useState } from 'react';
 import {
-  Alert,
-  Button,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
+	Alert,
+	Button,
+	EmitterSubscription,
+	Modal,
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
 } from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+import {
+	backupAccount,
+	importAccount,
+	setupLdk,
+	syncLdk,
+	getAddressBalance,
+	updateHeader,
+} from './ldk';
+import { connectToElectrum, subscribeToHeader } from './electrum';
+import ldk from '@synonymdev/react-native-ldk/dist/ldk';
+import lm, {
+	EEventTypes,
+	TChannelManagerPayment,
+	TChannelUpdate,
+} from '@synonymdev/react-native-ldk';
+import { peers } from './utils/constants';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
-const App = () => {
-  useEffect(() => {
-    const type = 'register';
-    PushNotificationIOS.addEventListener(type, (token) => {
-      alert(JSON.stringify(token))
-      console.log(token);
-    });
-    return () => {
-      PushNotificationIOS.removeEventListener(type);
-    };
-  });
+let paymentSubscription: EmitterSubscription | undefined;
+let onChannelSubscription: EmitterSubscription | undefined;
 
-  const onRegister = () => {
-    PushNotificationIOS.requestPermissions();
-  };
+const App = (): ReactElement => {
+	const [message, setMessage] = useState('...');
+	const [nodeStarted, setNodeStarted] = useState(false);
 
-  return (
-    <SafeAreaView style={{backgroundColor: 'blue', height: '100%'}}>
-      <StatusBar
-        barStyle={'light-content'}
-      />
-        <Text>LnPush</Text>
-        <Button title='Register' onPress={onRegister}/>
-    </SafeAreaView>
-  );
+	useEffect(() => {
+		//Restarting LDK on each code update causes constant errors.
+		if (nodeStarted) {
+			return;
+		}
+
+		(async (): Promise<void> => {
+			// Connect to Electrum Server
+			const electrumResponse = await connectToElectrum({});
+			if (electrumResponse.isErr()) {
+				setMessage(
+					`Unable to connect to Electrum Server:\n ${electrumResponse.error.message}`,
+				);
+				return;
+			}
+			// Subscribe to new blocks and sync LDK accordingly.
+			const headerInfo = await subscribeToHeader({
+				onReceive: async (): Promise<void> => {
+					const syncRes = await syncLdk();
+					if (syncRes.isErr()) {
+						setMessage(syncRes.error.message);
+						return;
+					}
+					setMessage(syncRes.value);
+				},
+			});
+			if (headerInfo.isErr()) {
+				setMessage(headerInfo.error.message);
+				return;
+			}
+			await updateHeader({ header: headerInfo.value });
+			// Setup LDK
+			const setupResponse = await setupLdk();
+			if (setupResponse.isErr()) {
+				setMessage(setupResponse.error.message);
+				return;
+			}
+
+			setNodeStarted(true);
+			setMessage(setupResponse.value);
+		})();
+	}, [nodeStarted]);
+
+	useEffect(() => {
+		if (!paymentSubscription) {
+			// @ts-ignore
+			paymentSubscription = ldk.onEvent(
+				EEventTypes.channel_manager_payment_claimed,
+				(res: TChannelManagerPayment) =>
+					alert(`Received ${res.amount_sat} sats`),
+			);
+		}
+
+		if (!onChannelSubscription) {
+			// @ts-ignore
+			onChannelSubscription = ldk.onEvent(
+				EEventTypes.new_channel,
+				(res: TChannelUpdate) =>
+					alert(
+						`Channel received from ${res.counterparty_node_id} Channel ${res.channel_id}`,
+					),
+			);
+		}
+
+		return (): void => {
+			paymentSubscription && paymentSubscription.remove();
+			onChannelSubscription && onChannelSubscription.remove();
+		};
+	}, []);
+
+	useEffect(() => {
+		const type = 'register';
+		PushNotificationIOS.addEventListener(type, (token) => {
+		  alert(JSON.stringify(token))
+		  console.log(token);
+	
+		  setMessage('Registered for push notifications');
+		});
+		
+		return () => {
+		  PushNotificationIOS.removeEventListener(type);
+		};
+	  });
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView
+				contentInsetAdjustmentBehavior="automatic"
+				style={styles.scrollView}>
+				<Text style={styles.text}>LN push</Text>
+				<View style={styles.messageContainer}>
+					<Text style={styles.text}>{message}</Text>
+				</View>
+				<View style={styles.container}>
+					<Button
+						title={'Get Node ID'}
+						onPress={async (): Promise<void> => {
+							const nodeIdRes = await ldk.nodeId();
+							if (nodeIdRes.isErr()) {
+								return setMessage(nodeIdRes.error.message);
+							}
+
+							Clipboard.setString(nodeIdRes.value);
+							console.log(nodeIdRes.value);
+
+							setMessage(`Node ID: ${nodeIdRes.value}`);
+						}}
+					/>
+
+					<Button
+						title={'Sync LDK'}
+						onPress={async (): Promise<void> => {
+							const syncRes = await syncLdk();
+							if (syncRes.isErr()) {
+								setMessage(syncRes.error.message);
+								return;
+							}
+							setMessage(syncRes.value);
+						}}
+					/>
+
+					<Button
+						title={'Add Peers'}
+						onPress={async (): Promise<void> => {
+							try {
+								const peersRes = await Promise.all(
+									Object.keys(peers).map(async (peer) => {
+										const addPeer = await lm.addPeer({
+											...peers[peer],
+											timeout: 5000,
+										});
+										if (addPeer.isErr()) {
+											setMessage(addPeer.error.message);
+											return;
+										}
+										return addPeer.value;
+									}),
+								);
+								setMessage(JSON.stringify(peersRes));
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
+
+					<Button
+						title={'List peers'}
+						onPress={async (): Promise<void> => {
+							try {
+								const listPeers = await ldk.listPeers();
+								if (listPeers.isErr()) {
+									setMessage(listPeers.error.message);
+									return;
+								}
+								setMessage(JSON.stringify(listPeers.value));
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
+
+					<Button
+						title={'List channels'}
+						onPress={async (): Promise<void> => {
+							try {
+								const listChannels = await ldk.listChannels();
+								if (listChannels.isErr()) {
+									setMessage(listChannels.error.message);
+									return;
+								}
+								if (listChannels.value.length < 1) {
+									setMessage('No channels detected.');
+									return;
+								}
+
+								let msg = '';
+								// Sort Channels
+								await Promise.all(
+									listChannels.value.map(async (channel) => {
+										const sorted = Object.keys(channel)
+											.sort()
+											.reduce((obj, key) => {
+												obj[key] = channel[key];
+												return obj;
+											}, {});
+										// Append channel info to msg.
+										await Promise.all(
+											Object.keys(sorted).map((key) => {
+												msg += `${key}: ${sorted[key]}\n`;
+											}),
+										);
+									}),
+								);
+
+								setMessage(msg);
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
+					
+					<Button
+						title={'Register for push notifications'}
+						onPress={async (): Promise<void> => {
+							PushNotificationIOS.requestPermissions();
+						}}
+					/> 
+					
+					<Button
+						title={'Create invoice'}
+						onPress={async (): Promise<void> => {
+							try {
+								const createPaymentRequest = await ldk.createPaymentRequest({
+									description: 'TODO push url',
+									expiryDeltaSeconds: 60 * 60,
+								});
+
+								if (createPaymentRequest.isErr()) {
+									setMessage(createPaymentRequest.error.message);
+									return;
+								}
+
+								const { to_str } = createPaymentRequest.value;
+								console.log(to_str);
+								Clipboard.setString(to_str);
+								setMessage(to_str);
+							} catch (e) {
+								setMessage(e.toString());
+							}
+						}}
+					/>
+
+					<Button
+						title={'Pay invoice'}
+						onPress={async (): Promise<void> => {
+							const paymentRequest = 'TODO';
+							const decode = await ldk.decode({ paymentRequest });
+							if (decode.isErr()) {
+								return setMessage(decode.error.message);
+							}
+
+							const { recover_payee_pub_key, amount_satoshis } = decode.value;
+
+							const ownAmountSats = 1000;
+							Alert.alert(
+								amount_satoshis
+									? `Pay ${amount_satoshis ?? 0}`
+									: 'Zero sat invoice found',
+								amount_satoshis
+									? `To pubkey: ${recover_payee_pub_key}`
+									: `Send ${ownAmountSats} sats (Our chosen amount) to send over?`,
+								[
+									{
+										text: 'Cancel',
+										onPress: () => console.log('Cancel Pressed'),
+										style: 'cancel',
+									},
+									{
+										text: 'Pay',
+										onPress: async (): Promise<void> => {
+											const pay = await lm.payWithTimeout({
+												paymentRequest,
+												amountSats: amount_satoshis ? undefined : ownAmountSats,
+												timeout: 20000,
+											});
+											if (pay.isErr()) {
+												return setMessage(pay.error.message);
+											}
+
+											setMessage(pay.value.payment_id);
+										},
+									},
+								],
+							);
+						}}
+					/>
+
+					<Button
+						title={'Show claimable balances for closed/closing channels'}
+						onPress={async (): Promise<void> => {
+							const balances = await ldk.claimableBalances(false);
+							if (balances.isErr()) {
+								return setMessage(balances.error.message);
+							}
+
+							setMessage(JSON.stringify(balances.value));
+						}}
+					/>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  background: {
-    color: 'red'
-  }
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	scrollView: {
+		flex: 1,
+	},
+	messageContainer: {
+		minHeight: 120,
+		marginHorizontal: 20,
+		justifyContent: 'center',
+	},
+	text: {
+		textAlign: 'center',
+	},
+	logModal: {
+		paddingTop: 40,
+		paddingHorizontal: 10,
+		flex: 1,
+		backgroundColor: 'black',
+	},
+	modalText: {
+		color: 'green',
+		fontSize: 10,
+	},
 });
 
 export default App;
---
+++ mobile/utils/constants.ts
@@ -0,0 +1,40 @@
+import { ENetworks, TAvailableNetworks } from '@synonymdev/react-native-ldk';
+
+export const selectedNetwork: TAvailableNetworks = 'bitcoinRegtest';
+
+//Lightning Peer Info
+export const peers = {
+	lnd: {
+		pubKey:
+			'0242c4451deb6266c2cf4593b891597529b73b1d2f98f7994a0ef420bfe1fd9b6c',
+		address: '127.0.0.1',
+		port: 9735,
+	},
+	// bt: {
+	// 	pubKey:
+	// 		'0296b2db342fcf87ea94d981757fdf4d3e545bd5cef4919f58b5d38dfdd73bf5c9',
+	// 	address: '34.79.58.84',
+	// 	port: 9735,
+	// },
+};
+
+//Electrum Server Info (Synonym Regtest Set By Default)
+export const customPeers = {
+	bitcoin: [
+		{
+			host: '35.187.18.233',
+			ssl: 8912,
+			tcp: 8911,
+			protocol: 'tcp',
+		},
+	],
+	bitcoinTestnet: [],
+	bitcoinRegtest: [
+		{
+			host: '192.168.0.105',
+			ssl: 18484,
+			tcp: 50001,
+			protocol: 'tcp',
+		},
+	],
+};
